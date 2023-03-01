6ddcdaa6ac1b296960fdf6c12317fec2ff5af4ea
Jacob-art-sketch <63834128+Jacob-art-sketch@users.noreply.github.com>
2023-03-02 00:58:02 +1300

    just testing out svelt

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<div class="topnav">
+    <div class="links">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+    </div>
+ <!-- for future use z index to create drop down menu find this symbol < once clicked should be inverted like this > -->
+  </div>
+  <style>
+    .topnav
+    {
+        height: 50px;
+        width: 100%;
+    }
+    .links
+    {
+        color: white;    
+        display: inline;
+        padding-left: 10px;
+    }
+    .links a
+    {
+        color:rgb(20, 20, 20);  
+        margin-right: 50px;
+        font-size: 20px;
+        text-decoration: none;
+    }
+    .links a:hover
+    {
+        color:rgb(21, 96, 225);
+    }
+  </style>
