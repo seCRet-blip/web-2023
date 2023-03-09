932558d70e191cb517d8f8e8ac27f6af63d81792
Jacob-art-sketch <63834128+Jacob-art-sketch@users.noreply.github.com>
2023-03-10 09:51:29 +1300

    adding a on click funtion to the nav bar links

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,33 +1,172 @@
-<div class="topnav">
-    <div class="links">
-        <a class="active" href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+   <!-- svelte-ignore a11y-missing-attribute -->
+   
+    <script>
+      const game = document.querySelector(".game");
+// get this working in the weekend
+      function displaytoConsol(){
+        console.log("hope this works");
+      }
+
+      
+    </script> 
+<nav>
+    <div class="menu-toggle">
+      <i class="fa fa-bars"></i>
     </div>
- <!-- for future use z index to create drop down menu find this symbol < once clicked should be inverted like this > -->
-  </div>
-  <style>
-    .topnav
-    {
-        height: 50px;
-        width: 100%;
-    }
-    .links
-    {
-        color: white;    
-        display: inline;
-        padding-left: 10px;
-    }
-    .links a
-    {
-        color:rgb(20, 20, 20);  
-        margin-right: 50px;
-        font-size: 20px;
-        text-decoration: none;
-    }
-    .links a:hover
-    {
-        color:rgb(21, 96, 225);
-    }
-  </style>
+    <ul class="menu">
+      <li><a class="game" href={'#'}>Games </a>
+        <ul class="sub-menu">
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+        </ul>
+      </li>
+      <li><a href={'#'}>Hardware</a>
+        <ul class="sub-menu">
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+        </ul>
+      </li>
+      <li><a href={'#'}>Services</a>
+        <ul class="sub-menu">
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+        </ul>
+      </li>
+      <li><a href={'#'}>News</a>
+        <ul class="sub-menu">
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+        </ul></li>
+      <li><a href={'#'}>Shop</a>
+        <ul class="sub-menu">
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+          <li><a href={'#'}>Blank</a></li>
+        </ul></li>
+      <li><a href={'#'}>Support</a>
+        <ul class="sub-menu">
+        <li><a href={'#'}>Blank</a></li>
+        <li><a href={'#'}>Blank</a></li>
+        <li><a href={'#'}>Blank</a></li>
+      </ul></li>
+    </ul>
+</nav>
+<style>
+    nav{
+  background-color: #fff;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.menu-toggle {
+  display: none;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.menu {
+  display: flex;
+  justify-content: space-between;
+  margin: 0;
+  padding-right: 75%;
+  list-style: none;
+}
+
+.menu li {
+  position: relative;
+  font-size: 20px;
+}
+
+.menu li a {
+  display: block;
+  padding: 10px;
+  color: #444;
+  text-decoration: none;
+}
+
+.menu li:hover > a {
+  color: #045dbb;
+}
+
+.sub-menu {
+  display: none;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  z-index: 1;
+  background-color: white;
+}
+
+.sub-menu li {
+  display: block;
+}
+
+.sub-menu li a {
+  display: block;
+  padding: 10px;
+  color: #444;
+  text-decoration: none;
+}
+
+.menu li:hover > .sub-menu {
+  display: block;
+  animation: fadeInDown 0.5s ease;
+}
+
+@keyframes fadeInDown {
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+@media screen and (max-width: 768px) {
+  .menu-toggle {
+    display: block;
+  }
+  
+  .menu {
+    display: none;
+    flex-direction: column;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    background-color: #333;
+    margin: 0;
+  }
+  
+  .menu li {
+    display: block;
+    border-bottom: 1px solid #444;
+  }
+  
+  .menu li:last-child {
+    border-bottom: none;
+  }
+  
+  .menu li a {
+    display: block;
+    padding: 10px;
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  .menu li:hover > .sub-menu {
+    display: block;
+    position: static;
+    background-color: #444;
+  }
+}
+
+</style>
+  
