64ab55883f6b9d092b1f2f7434a82ce41ece242b
Jacob-art-sketch <63834128+Jacob-art-sketch@users.noreply.github.com>
2023-05-13 14:23:02 +1200

    just playing around with the test component so i can add it to the nav bar later

--- my-app/src/routes/test.svelte
+++ my-app/src/routes/test.svelte
@@ -1,24 +1,41 @@
 <script>
-    import { tweened } from 'svelte/motion';
-    import { cubicOut } from 'svelte/easing';
-  
-    let moveDiv = false;
-    const marginTop = tweened(20, { duration: 500, easing: cubicOut });
-  
-    function moveDivDown() {
-      moveDiv = !moveDiv;
-      marginTop.set(moveDiv ? 40 : 20);
+  import { tweened } from 'svelte/motion';
+  import { cubicOut } from 'svelte/easing';
+
+  let moveDiv = false;
+  const marginTop = tweened(20, { duration: 500, easing: cubicOut });
+  let x;
+
+  function moveDivDown() {
+    moveDiv = !moveDiv;
+    marginTop.set(moveDiv ? 40 : 20);
+  }
+
+  $: if (x) {
+    if (moveDiv) {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
     }
-  </script>
-  
-  <style>
-    .box {
-      width: 100px;
-      height: 100px;
-      background-color: lightblue;
-    }
-  </style>
-  
-  <a href="/" on:click|preventDefault={moveDivDown}>Click me to move the div down</a>
-  <div class="box" style="margin-top: {$marginTop}px">I'm a moving div!</div>
-  
+  }
+</script>
+
+<style>
+  .box {
+    display: none;
+    width: 100px;
+    height: 100px;
+    background-color: lightblue;
+  }
+</style>
+
+<a href="#" on:click|preventDefault={moveDivDown}>Click me to move the div down</a>
+<div class="box" bind:this={x} style="margin-top: {$marginTop}px">
+
+
+<a href="/">test</a>
+<a href="/">test</a>
+<a href="/">test</a>
+<a href="/">test</a>
+
+</div>
