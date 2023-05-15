faf5f74f1d15c63682880150c8e3b866a723bde2
Jacob-art-sketch <63834128+Jacob-art-sketch@users.noreply.github.com>
2023-05-16 11:55:23 +1200

    adding more links and trying to limit the code being dry

--- my-app/src/routes/test.svelte
+++ my-app/src/routes/test.svelte
@@ -1,41 +1,57 @@
 <script>
+  import { writable } from 'svelte/store';
   import { tweened } from 'svelte/motion';
   import { cubicOut } from 'svelte/easing';
 
-  let moveDiv = false;
-  const marginTop = tweened(20, { duration: 500, easing: cubicOut });
-  let x;
+  let dropdownItems = [
+    {id: 1, title: 'Games', links: ['News', 'Testing', 'PS plus']},
+    {id: 2, title: 'Support', links: ['Support', 'IT Support', 'PS Status']},
+    // Add more items as needed
+  ];
 
-  function moveDivDown() {
-    moveDiv = !moveDiv;
-    marginTop.set(moveDiv ? 40 : 20);
-  }
+  const dropdownStates = writable({});
+  const marginTop = tweened(20, { duration: 200, easing: cubicOut });
 
-  $: if (x) {
-    if (moveDiv) {
-      x.style.display = 'block';
-    } else {
-      x.style.display = 'none';
-    }
+  let x = [];
+
+  function moveDivDown(id, index) {
+    dropdownStates.update(states => {
+      if (states[id]) {
+        x[index].style.display = 'none';
+        marginTop.set(20);
+        states[id] = false;
+      } else {
+        x[index].style.display = 'block';
+        marginTop.set(40);
+        states[id] = true;
+      }
+      return states;
+    });
   }
 </script>
 
 <style>
   .box {
     display: none;
-    width: 100px;
+    width: 100%;
     height: 100px;
     background-color: lightblue;
   }
+  .dropDownContent{
+    display: inline;
+  }
+  .dropDownContent a{
+    padding-left: 15px;
+  }
 </style>
 
-<a href="#" on:click|preventDefault={moveDivDown}>Click me to move the div down</a>
-<div class="box" bind:this={x} style="margin-top: {$marginTop}px">
-
-
-<a href="/">test</a>
-<a href="/">test</a>
-<a href="/">test</a>
-<a href="/">test</a>
-
-</div>
+{#each dropdownItems as item, index (item.id)}
+  <a href="/" on:click|preventDefault={() => moveDivDown(item.id, index)}>{item.title}</a>
+  <div class="box" bind:this={x[index]} style="margin-top: {$marginTop}px">
+    <div class="dropDownContent">
+      {#each item.links as link}
+        <a href="/">{link}</a>
+      {/each}
+    </div>
+  </div>
+{/each}
