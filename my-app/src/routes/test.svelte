af413088b778725ea56bd7a8c59cf0a4dadd3dc9
Jacob-art-sketch <63834128+Jacob-art-sketch@users.noreply.github.com>
2023-05-19 16:34:42 +1200

    adding style to the drop down menu

--- my-app/src/routes/test.svelte
+++ my-app/src/routes/test.svelte
@@ -5,8 +5,12 @@
 
   let dropdownItems = [
     {id: 1, title: 'Games', links: ['News', 'Testing', 'PS plus']},
-    {id: 2, title: 'Support', links: ['Support', 'IT Support', 'PS Status']}, 
-    {id: 3, title: 'Support', links: ['Support', 'IT Support', 'PS Status']}, 
+    {id: 2, title: 'Hardware', links: ['PS5', 'PS4', 'PS VR2']}, 
+    {id: 3, title: 'Services', links: ['PS Plus', 'PS Stars']}, 
+    {id: 4, title: 'News', links: ['PS Blog', 'This month on playstation']}, 
+    {id: 5, title: 'Shop', links: ['Support', 'IT Support', 'PS Status']}, 
+    {id: 6, title: 'Support', links: ['Support', 'IT Support', 'PS Status']}, 
+     
     // Add more items as needed
   ];
 
@@ -15,27 +19,35 @@
 
   let x = [];
 
-  function moveDivDown(id, index) {
+  function moveDivDown(id) {
     dropdownStates.update(states => {
+      // Close all other dropdowns
+      for (let key in states) {
+        if (states[key] && key != id) {
+          x[key-1].style.display = 'none'; // keys start from 1
+          states[key] = false;
+        }
+      }
+
+      // Toggle the current dropdown
       if (states[id]) {
-        x[index].style.display = 'none';
+        x[id-1].style.display = 'none'; // ids start from 1
         marginTop.set(20);
         states[id] = false;
       } else {
-        x[index].style.display = 'block';
+        x[id-1].style.display = 'block'; // ids start from 1
         marginTop.set(40);
         states[id] = true;
       }
+
       return states;
     });
   }
 </script>
 
-
-
 <div class="links">
-  {#each dropdownItems as item, index (item.id)}
-    <a href="/" on:click|preventDefault={() => moveDivDown(item.id, index)}>{item.title}</a>
+  {#each dropdownItems as item (item.id)}
+    <a class="MainLinks" href="/" on:click|preventDefault={() => moveDivDown(item.id)}>{item.title}</a>
   {/each}
 </div>
 
@@ -49,18 +61,30 @@
   </div>
 {/each}
 
-
 <style>
+.MainLinks{
+  color: black;
+  text-decoration: none;
+  margin-left: 15px;
+}
+
+
   .box {
     display: none;
     width: 100%;
     height: 100px;
     background-color: lightblue;
   }
-  .dropDownContent{
-    display: inline;
-  }
-  .dropDownContent a{
-    padding-left: 15px;
-  }
+  .dropDownContent {
+  display: inline-block;
+  margin-left: 40%;
+  margin-right: 25%;
+  padding-top: 40px; /* Add this line */
+}
+.dropDownContent a {
+  padding-left: 15px;
+  color: black;
+  
+}
+
 </style>
