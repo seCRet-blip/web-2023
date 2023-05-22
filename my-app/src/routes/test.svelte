6ae3f9cf0d8bafb68299c48096255130d6869717
Jacob-art-sketch <63834128+Jacob-art-sketch@users.noreply.github.com>
2023-05-23 09:58:14 +1200

    now have the drop down displaying above everything and have the correct svg

--- my-app/src/routes/test.svelte
+++ my-app/src/routes/test.svelte
@@ -2,65 +2,80 @@
   import { writable } from 'svelte/store';
   import { tweened } from 'svelte/motion';
   import { cubicOut } from 'svelte/easing';
-
   let dropdownItems = [
-    {id: 1, title: 'Games', links: ['News', 'Testing', 'PS plus']},
-    {id: 2, title: 'Hardware', links: ['PS5', 'PS4', 'PS VR2']}, 
-    {id: 3, title: 'Services', links: ['PS Plus', 'PS Stars']}, 
-    {id: 4, title: 'News', links: ['PS Blog', 'This month on playstation']}, 
-    {id: 5, title: 'Shop', links: ['Support', 'IT Support', 'PS Status']}, 
-    {id: 6, title: 'Support', links: ['Support', 'IT Support', 'PS Status']}, 
-     
-    // Add more items as needed
-  ];
-  let rotated = false;
+  {id: 1, title: 'Games', links: ['News', 'Testing', 'PS plus']},
+  {id: 2, title: 'Hardware', links: ['PS5', 'PS4', 'PS VR2']}, 
+  {id: 3, title: 'Services', links: ['PS Plus', 'PS Stars']}, 
+  {id: 4, title: 'News', links: ['PS Blog', 'This month on Playstation']}, 
+  {id: 5, title: 'Shop', links: ['Support', 'IT Support', 'PS Status']}, 
+  {id: 6, title: 'Support', links: ['Support', 'IT Support', 'PS Status']}
+  // Add more items as needed
+];
+
+const rotationStates = writable(dropdownItems.map(() => false));
   const dropdownStates = writable({});
   const marginTop = tweened(20, { duration: 200, easing: cubicOut });
 
   
   let x = [];
 
-  function moveDivDown(id) {
-    rotated = !rotated;
-    dropdownStates.update(states => {
-      // Close all other dropdowns
-      for (let key in states) {
-        if (states[key] && key != id) {
-          x[key-1].style.display = 'none'; // keys start from 1
-          states[key] = false;
-        }
-      }
+  let rotationStatesVal;
+rotationStates.subscribe(val => rotationStatesVal = val);
 
-      // Toggle the current dropdown
-      if (states[id]) {
-        x[id-1].style.display = 'none'; // ids start from 1
-        marginTop.set(20);
-        states[id] = false;
-      } else {
-        x[id-1].style.display = 'block'; // ids start from 1
-        marginTop.set(40);
-        states[id] = true;
-      }
+function moveDivDown(id) {
+  dropdownStates.update(states => {
+  
+    // Toggle the current dropdown
+    if (states[id]) {
+      x[id-1].style.display = 'none'; // ids start from 1
+      x[id-1].style.zIndex = '0'; // Reset zIndex when hidden
+      marginTop.set(20);
+      states[id] = false;
+      rotationStates.update(rs => {
+        rs[id-1] = false;
+        return rs;
+      });
+    } else {
+      x[id-1].style.display = 'block'; // ids start from 1
+      x[id-1].style.zIndex = '1'; // Increase zIndex when displayed
+      marginTop.set(40);
+      states[id] = true;
+      rotationStates.update(rs => {
+        rs[id-1] = true;
+        return rs;
+      });
+    }
 
-      return states;
-    });
-  }
+    return states;
+  });
+}
+
+
+  let dataArray = [];
+
+
+
+
 </script>
 
 <div class="links">
-  {#each dropdownItems as item (item.id)}
-    <a class="MainLinks" href="/" on:click|preventDefault={() => moveDivDown(item.id)}>   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-           stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-           class:rotated={rotated} class="feather feather-arrow-down dropdown-icon"
-           id="dropdown-icon">
-        <line x1="12" y1="5" x2="12" y2="19"></line>
-        <polyline points="19 12 12 19 5 12"></polyline>
-      </svg>{item.title}</a>
-  {/each}
+  {#each dropdownItems as item, index (item.id)}
+  <a class="MainLinks" href="/" on:click|preventDefault={() => moveDivDown(item.id)}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"   
+class:rotated={rotationStatesVal[index]} class="feather feather-arrow-down dropdown-icon"
+id="dropdown-icon">
+<polyline 
+points="6 9 12 15 18 9"></polyline
+></svg>{item.title}
+  </a>
+{/each}
+
+
 </div>
 
 {#each dropdownItems as item, index (item.id)}
-  <div class="box" bind:this={x[index]} style="margin-top: {$marginTop}px">
+  <div class="box" bind:this={x[index]} >
     <div class="dropDownContent">
       {#each item.links as link}
         <a href="/">{link}</a>
@@ -69,7 +84,10 @@
   </div>
 {/each}
 
+
 <style>
+
+
   .dropdown-icon {
   transition: transform 0.3s ease-in-out;
 }
@@ -84,23 +102,35 @@
   margin-left: 15px;
 }
 
+.box {
+  position: absolute; /* Add this line */
+  width: 100%;
+  height: 100px;
+  background-color: white;
+  display: none;
+  z-index: 1; /* Add this line */
+  box-shadow:
+      0 1px 1px hsl(0deg 0% 0% / 0.075),
+      0 2px 2px hsl(0deg 0% 0% / 0.075),
+      0 4px 4px hsl(0deg 0% 0% / 0.075),
+      0 8px 8px hsl(0deg 0% 0% / 0.075),
+      0 16px 16px hsl(0deg 0% 0% / 0.075)
+    ;
+}
 
-  .box {
-    display: none;
-    width: 100%;
-    height: 100px;
-    background-color: lightblue;
-  }
-  .dropDownContent {
+.dropDownContent {
   display: inline-block;
   margin-left: 40%;
   margin-right: 25%;
-  padding-top: 40px; /* Add this line */
+  padding-top: 40px; 
+  border-bottom: 5px solid black;
 }
+
 .dropDownContent a {
   padding-left: 15px;
   color: black;
-  
 }
+
+
 
 </style>
